77d706331a804a9fb10764b4390abf2efda2f4b1
AbdelrhmanNile <abdelrhmannile@gmail.com>
2022-11-05 17:25:46 +0200

    formatting

--- auto_ricer/autoricer.py
+++ auto_ricer/autoricer.py
@@ -3,54 +3,66 @@ import os
 import typer
 from time import sleep
 
-config_folder = f"/home/{os.getlogin()}/.config/auto-ricer"
-cli = typer.Typer(add_completion=True)
+gh_repo = "https://github.com/AbdelrhmanNile/auto-ricer.git"
+
+user = os.getlogin()
+config_dir = f"/home/{user}/.config/auto-ricer"
+bspwmrc_path = f"/home/{user}/.config/bspwm/bspwmrc"
+pywal_cache_dir = f"/home/{user}/.cache/wal"
+
+cli = typer.Typer()
 
 
 @cli.command()
 def from_img(wallpaper: str):
 
-    if not os.path.isdir(config_folder):
+    if not os.path.isdir(config_dir):
         install_theme()
+
+    typer.echo("Extracting colors from the wallpaper...")
     colors_from_img = pywal.colors.get(wallpaper)
-
     colors_list = list(colors_from_img["colors"].values())
-
     final_colors = pywal.colors.colors_to_dict(colors_list, wallpaper)
     pywal.export.every(final_colors)
+
+    typer.echo("Setting color scheme...")
     pywal.sequences.send(final_colors)
-
     pywal.reload.xrdb()
-
-    os.system(f"sh /home/{os.getlogin()}/.config/bspwm/bspwmrc")
+    os.system(f"sh {bspwmrc_path} > /dev/null")
     sleep(1)
+
+    typer.echo("Setting wallpaper...")
     pywal.wallpaper.set_wm_wallpaper(wallpaper)
+    os.system(
+        f"sed -i '/feh/d' {bspwmrc_path} && echo 'feh --bg-fill {wallpaper} &' >> {bspwmrc_path}"
+    )
 
+    typer.echo("Generating GTK3 theme...")
     fix_gtk_colors()
-
-    oomox_file_path = f"/home/{os.getlogin()}/.cache/wal/colors-oomox"
-    gtk_theme_command = f"cd {config_folder}/auto-ricer/gtk/theme_materia/materia-theme && chmod +x change_color.sh && ./change_color.sh -o auto-ricer -t /home/{os.getlogin()}/.themes {oomox_file_path}"
+    oomox_file_path = f"{pywal_cache_dir}/colors-oomox"
+    gtk_theme_command = f"cd {config_dir}/auto-ricer/gtk/theme_materia/materia-theme && chmod +x change_color.sh && ./change_color.sh -o auto-ricer -t /home/{user}/.themes {oomox_file_path} > {config_dir}/theme_log.txt"
     os.system(gtk_theme_command)
 
-    icons_theme_command = f"cd {config_folder}/auto-ricer/gtk/icons_papirus && chmod +x change_color.sh && ./change_color.sh -o auto-ricer -d /home/{os.getlogin()}/.icons/auto-ricer {oomox_file_path}"
+    typer.echo("Generating icons theme...")
+    icons_theme_command = f"cd {config_dir}/auto-ricer/gtk/icons_papirus && chmod +x change_color.sh && ./change_color.sh -o auto-ricer -d /home/{user}/.icons/auto-ricer {oomox_file_path} > {config_dir}/icons_log.txt"
     os.system(icons_theme_command)
 
 
 def fix_gtk_colors():
-    oomox_colors = open(f"/home/{os.getlogin()}/.cache/wal/colors-oomox", "r").read()
+    oomox_colors = open(f"{pywal_cache_dir}/colors-oomox", "r").read()
     oomox_colors = oomox_colors.splitlines()
     bg_color = oomox_colors[1].strip("BG=")
     oomox_colors[9] = f"BTN_BG={bg_color}"
     oomox_colors = "\n".join(oomox_colors)
 
-    with open(f"/home/{os.getlogin()}/.cache/wal/colors-oomox", "w") as f:
+    with open(f"{pywal_cache_dir}/colors-oomox", "w") as f:
         f.write(oomox_colors)
 
 
 def install_theme():
-    os.system(
-        f"mkdir {config_folder} && cd {config_folder} && git clone https://github.com/AbdelrhmanNile/auto-ricer.git"
-    )
+
+    typer.echo("Downloading gtk theme and icons theme...")
+    os.system(f"mkdir {config_dir} && cd {config_dir} && git clone {gh_repo}")
 
 
 if __name__ == "__main__":
