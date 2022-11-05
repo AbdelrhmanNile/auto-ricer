9357d1f13dbe01c0e8ef1912badc18d046d5ef3d
AbdelrhmanNile <abdelrhmannile@gmail.com>
2022-11-05 17:35:54 +0200

    formatting

--- auto_ricer/autoricer.py
+++ auto_ricer/autoricer.py
@@ -19,33 +19,35 @@ def from_img(wallpaper: str):
     if not os.path.isdir(config_dir):
         install_theme()
 
-    typer.echo("Extracting colors from the wallpaper...")
+    typer.echo("\nExtracting colors from the wallpaper...\n")
     colors_from_img = pywal.colors.get(wallpaper)
     colors_list = list(colors_from_img["colors"].values())
     final_colors = pywal.colors.colors_to_dict(colors_list, wallpaper)
     pywal.export.every(final_colors)
 
-    typer.echo("Setting color scheme...")
+    typer.echo("\nSetting color scheme...\n")
     pywal.sequences.send(final_colors)
     pywal.reload.xrdb()
     os.system(f"sh {bspwmrc_path} > /dev/null")
     sleep(1)
 
-    typer.echo("Setting wallpaper...")
+    typer.echo("\nSetting wallpaper...\n")
     pywal.wallpaper.set_wm_wallpaper(wallpaper)
     os.system(
         f"sed -i '/feh/d' {bspwmrc_path} && echo 'feh --bg-fill {wallpaper} &' >> {bspwmrc_path}"
     )
 
-    typer.echo("Generating GTK3 theme...")
+    typer.echo("\nGenerating GTK3 theme...\n")
     fix_gtk_colors()
     oomox_file_path = f"{pywal_cache_dir}/colors-oomox"
     gtk_theme_command = f"cd {config_dir}/auto-ricer/gtk/theme_materia/materia-theme && chmod +x change_color.sh && ./change_color.sh -o auto-ricer -t /home/{user}/.themes {oomox_file_path} > {config_dir}/theme_log.txt"
     os.system(gtk_theme_command)
 
-    typer.echo("Generating icons theme...")
+    typer.echo("\nGenerating icons theme...\n")
     icons_theme_command = f"cd {config_dir}/auto-ricer/gtk/icons_papirus && chmod +x change_color.sh && ./change_color.sh -o auto-ricer -d /home/{user}/.icons/auto-ricer {oomox_file_path} > {config_dir}/icons_log.txt"
     os.system(icons_theme_command)
+
+    typer.echo("Done!")
 
 
 def fix_gtk_colors():
@@ -61,7 +63,7 @@ def fix_gtk_colors():
 
 def install_theme():
 
-    typer.echo("Downloading gtk theme and icons theme...")
+    typer.echo("\nDownloading gtk theme and icons theme...\n")
     os.system(f"mkdir {config_dir} && cd {config_dir} && git clone {gh_repo}")
 
 
